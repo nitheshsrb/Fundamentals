2629800a6260539e267b37e6c48aaca36e60a1a9
Nithesh <nitheshbharadwaj45@gmail.com>
2026-07-03 19:33:01 +0100

    Reached training and evaluation

--- weather_data/model_metric.py
+++ weather_data/model_metric.py
@@ -2,44 +2,49 @@ from sklearn.metrics import r2_score,mean_absolute_error,root_mean_squared_error
 
 import pandas as pd
 import joblib
+from train_pipeline import split_data
 
-model = joblib.load('weather_data/models/test_models/Baseline_Regression_Model_v1')
-#print(model.summary())
-X_test = pd.read_csv('weather_data/data/Holdout_independent_data.csv')
-X_test = X_test.iloc[:,1:]
-for c in X_test.columns:
-    X_test[c] = X_test[c].astype('float')
-print('Datatypes of X_test are :',X_test.dtypes)
-y_test = pd.read_csv('weather_data/data/Holdout_dependent_data.csv')
-y_test = y_test.iloc[:,1:]
-print(y_test.head())
-y_test['temperature_2m_max'] = pd.to_numeric(y_test['temperature_2m_max'],errors='coerce')
-y_pred = model.predict(X_test)
+def evaluation(df):
+    model = joblib.load('weather_data/models/test_models/Baseline_Regression_Model_v1')
+    #print(model.summary())
+    selected_features = model.params.index
+    X_train,y_train,X_test,y_test,cutoff_date = split_data(df,selected_features)
+    for c in X_test.columns:
+        X_test[c] = X_test[c].astype('float')
 
-dates_df = pd.read_csv('weather_data/data/Feature_Engineered_data.csv')
-dates_df['date'] = pd.to_datetime(dates_df['date'],format = '%Y-%m-%d')
+    y_test = pd.to_numeric(y_test,errors='coerce')
+    y_pred = model.predict(X_test)
 
-max_date = dates_df['date'].max()
-cutoff_date = max_date - pd.DateOffset(years = 1)
+    print(y_test,y_pred)
 
-print(dates_df.head(5))
+    dates_df = df
+    dates_df['date'] = pd.to_datetime(dates_df['date'],format = '%Y-%m-%d')
 
-dates_df_shrunk = pd.DataFrame(dates_df['date'][dates_df['date'] >= cutoff_date]).reset_index()
-print(dates_df_shrunk.head())
+    max_date = dates_df['date'].max()
+    cutoff_date = max_date - pd.DateOffset(years = 1)
 
-Results = pd.DataFrame()
-Results['date'] = dates_df_shrunk['date']
-Results['Actuals'] = y_test['temperature_2m_max']
-Results['Predictions'] = y_pred
+    print(dates_df.head(5))
 
+    dates_df_shrunk = pd.DataFrame(dates_df['date'][dates_df['date'] >= cutoff_date]).reset_index()
+    print(dates_df_shrunk.head())
 
-print("The number of generated predictions are", len(y_pred))
-print("R2 score is : ",r2_score(y_test,y_pred))
-print("Mean absolute error is : ", mean_absolute_error(y_test,y_pred))
-
-print('Results are : ',Results)
+    Results = pd.DataFrame()
+    Results['date'] = dates_df_shrunk['date']
+    Results['Actuals'] = y_test.reset_index(drop=True)
+    Results['Predictions'] = y_pred.reset_index(drop=True)
 
 
+    print("The number of generated predictions are", len(y_pred))
+    print("R2 score is : ",r2_score(y_test,y_pred))
+    print("Mean absolute error is : ", mean_absolute_error(y_test,y_pred))
+
+    print('Results are : ',Results)
+
+
+if __name__ == "__main__":
+
+    df = pd.read_csv('weather_data/data/Feature_engineered_data.csv')
+    evaluation(df)
 
 
 
--- weather_data/train_pipeline.py
+++ weather_data/train_pipeline.py
@@ -7,17 +7,14 @@ import joblib
 import statsmodels.api as sm
 import os
 
-def train_pipeline(df):
-    selected_features = ['max_temp_lag1',
-                         'cloud_cover_mean (%)']
-    
-    df['date'] = pd.to_datetime(df['date'],format = '%Y-%m-%d')
+def split_data(df,selected_features):
 
+    df['date'] = pd.to_datetime(df['date'],format = '%Y-%m-%d')
     max_date = df['date'].max()
     cutoff_date = max_date - pd.DateOffset(years = 1) 
 
-    print("The cutoff date is : ",cutoff_date)
-    print("After cutoff dataframe is : ",df[df['date']>= cutoff_date])
+    print("The training cutoff date is : ",cutoff_date)
+    #print("After cutoff dataframe is : ",df[df['date']>= cutoff_date])
     
     X_train = df[selected_features][df['date'] < cutoff_date]
     X_test = df[selected_features][df['date'] >= cutoff_date]
@@ -25,33 +22,29 @@ def train_pipeline(df):
     y_train = pd.to_numeric(df['temperature_2m_max'][df['date'] < cutoff_date],errors='coerce')
     y_test = df['temperature_2m_max'][df['date'] >= cutoff_date]
 
-   # scaler = StandardScaler()
-    #X_train = scaler.fit_transform(X_train)
-    #X_test = scaler.fit_transform(X_test)
+    return X_train,y_train,X_test,y_test,cutoff_date
 
-    #joblib.dump(scaler,'weather_data/models/Linear_Standard_Scaler_v1')
-
+def train_baseline_pipeline(df):
+    
+    selected_features = ['max_temp_lag1',
+                         'cloud_cover_mean (%)',
+                         'dew_point_2m_mean']
+    
+    X_train,y_train,X_test,y_test,cutoff_date = split_data(df,selected_features)
     #if os.path.exists('weather_data/models/Linear_Model_v1') == False:
     res = sm.OLS(y_train,X_train)#.fit()
-    res = res.fit_regularized(alpha = 1,L1_wt=1,method = 'elastic_net')
+    res = res.fit_regularized(alpha = 0.5,L1_wt=1,method = 'elastic_net')
     joblib.dump(res,'weather_data/models/test_models/Baseline_Regression_Model_v1')
     print("Model successfully written")
+
+    y_train_pred = res.predict(X_train)
+    print("Training R2 : ",r2_score(y_train,y_train_pred))
+    #print(res.summary())
     #else:
     #    pass
-    for c in X_train.columns:
-        X_train[c] = pd.to_numeric(X_train[c],errors = 'coerce')
-    training_preds = res.predict(X_train)
-    print("Training R2 is : ",r2_score(y_train,training_preds))
-
-    X_test.to_csv('weather_data/data/Holdout_independent_data.csv')
-    y_test.to_csv('weather_data/data/Holdout_dependent_data.csv')
-
-    VIF = pd.DataFrame()
-    VIF["feature"] = X_train.columns
-    VIF["VIF"] = [variance_inflation_factor(X_train.values,i) for i in range(len(X_train.columns))]
-    print(VIF)
+    
 
 if __name__ == "__main__":
 
     df = pd.read_csv('weather_data/data/Feature_engineered_data.csv')
-    model = train_pipeline(df)
+    train_baseline_pipeline(df)
